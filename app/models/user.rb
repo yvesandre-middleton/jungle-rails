219fe83e4d9603baf94fb52d7497b0c16f0ac4d2
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-03-06 19:10:27 +0000

    added home pages and product details spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
             :confirmation => true
 
 
-    def authenticate_with_credentials(email, password)
+    def self.authenticate_with_credentials(email, password)
       user = User.find_by_email(email.downcase.strip)
       if user && user.authenticate(password)
         user
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+ scenario "Navigates from home page to product detail page" do
+  # ACT
+  visit root_path
+  within first('.product') do
+    click_link 'Details'
+  end
+
+  # DEBUG
+  # sleep 5
+  # puts 'hello'
+  # puts page.html
+
+
+  # VERIFY
+  expect(page).to have_content('Reviews')
+  save_screenshot
+end
+
+end
